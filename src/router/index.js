051eeac808c719bfe8f0da31f10361bf74c745ca
KeithChou <keithchou95@gmail.com>
2017-10-14 17:14:06 +0800

    增加Router文件目录

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+class Router extends React.Component {
+  render () {
+    return (
+      <Router>
+      </Router>
+    )
+  }
+}
+
+export default Router
